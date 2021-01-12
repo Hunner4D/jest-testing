1fa8a302772fe68cce0fce90f27866e84e548118
Hunner4D <h.herrera.academics@gmail.com>
2021-01-11 18:39:11 -0800

    update CommentBox tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,9 +18,8 @@ afterEach(() => {
 });
 
 it("has a text area and a button", () => {
-  expect(
-    wrapped.find("textarea").length && wrapped.find("button").length
-  ).toEqual(1);
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(2);
 });
 
 describe("the text area", () => {
